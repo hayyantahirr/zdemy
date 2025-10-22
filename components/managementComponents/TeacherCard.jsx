0ecf5617e0bf716358c8d01f016146927d608d97
hayyantahirr <hayyantahir7@gmail.com>
2025-10-22 21:11:27 +0500

    Small changes made

--- components/managementComponents/TeacherCard.jsx
+++ components/managementComponents/TeacherCard.jsx
@@ -114,13 +114,13 @@ const TeacherCard = ({ teacher, delay = 0 }) => {
                     <span className="font-semibold text-[#FF725E] text-lg">
                       A*:
                     </span>
-                    <p className="ml-2 text-lg">{teacher.aGrade || "0"}+</p>
+                    <p className="ml-2 text-lg">{teacher.aStar || "0"} +</p>
                   </div>
                   <div className="flex items-center">
                     <span className="font-semibold text-[#FF725E] text-lg">
                       A:
                     </span>
-                    <p className="ml-2 text-lg">{teacher.aStar || "0"}+</p>
+                    <p className="ml-2 text-lg">{teacher.aGrade || "0"}+</p>
                   </div>
                 </div>
                 <div className="flex gap-6 justify-center mt-5">
